7080bd3e6548285d4c8f40b7bd376aa4ce6103ec
KwintenP <kwinten.pisman@gmail.com>
2017-12-22 21:04:36 +0100

    Cleaned up the example schematic.

--- examples/src/inline-template-component/index.ts
+++ examples/src/inline-template-component/index.ts
@@ -1,17 +1,6 @@
-import {chain, externalSchematic, Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
+import {externalSchematic, Rule} from '@angular-devkit/schematics';
 
 export default function (options: any): Rule {
     let updatedOptions: any = {...options, inlineTemplate: true};
-    return chain(
-        [
-            externalSchematic('@schematics/angular', 'component', updatedOptions),
-            (tree: Tree, context: SchematicContext) => {
-                // Show the options for this Schematics.
-                context.logger.info('My Other Schematic: ' + JSON.stringify(options));
-                debugger;
-                options = {...options, inlineTemplate: true};
-                return tree;
-            },
-        ]
-    );
+    return externalSchematic('@schematics/angular', 'component', updatedOptions)
 }

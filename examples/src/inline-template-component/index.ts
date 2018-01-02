5a89b07776b9106873299143d9e305c2743049de
KwintenP <kwinten.pisman@gmail.com>
2018-01-02 19:39:27 +0100

    cleaned everything up

--- examples/src/inline-template-component/index.ts
+++
@@ -1,6 +0,0 @@
-import {externalSchematic, Rule} from '@angular-devkit/schematics';
-
-export default function (options: any): Rule {
-    let updatedOptions: any = {...options, inlineTemplate: true};
-    return externalSchematic('@schematics/angular', 'component', updatedOptions)
-}
